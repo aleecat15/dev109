c39c0b3dcaefd1d020d1564037037f8c9746d678
aleecat15 <166102492+aleecat15@users.noreply.github.com>
2024-05-18 21:57:56 -0700

    multiple-objects.js

--- m07/javascript/multiple-objects.js
+++ m07/javascript/multiple-objects.js
@@ -27,7 +27,7 @@ var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
 var details3 = newHotel.name + ' rooms: ';
-    details3 += parkHotel.checkAvailability();
+    details3 += newHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details3;
 
