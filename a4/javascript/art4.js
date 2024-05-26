a8352ed9918294808555892dba64e536b5bf1ad3
aleecat15 <166102492+aleecat15@users.noreply.github.com>
2024-05-25 20:42:13 -0700

    art4.js

--- a4/javascript/art4.js
+++ a4/javascript/art4.js
@@ -21,12 +21,9 @@ function firstName(){
     //3) Do validation
     if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
-    else if (firstname==="null" || firstname==="" || firstname.length < 0 ) {
-        errorMessages += "<p>The first name is required</p>";
-        console.log("First name invalid — length")
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid character in first name (accepts only A-Z, a-z, and ,.'-)</p>";
-            console.log("First name invalid — bad characters")
+        errorMessages += "<p>Invalid character in first name (accepts only A-Z, a-z, and ,.'-)</p>";
+        console.log("First name invalid — bad characters")
         }
         } else {
                 validFirstname = true;
