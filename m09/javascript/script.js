dff095eb4fe336a88255070cbd2b2a6752e15bb5
aleecat15 <166102492+aleecat15@users.noreply.github.com>
2024-05-30 20:24:15 -0700

    script.js

--- m09/javascript/script.js
+++ m09/javascript/script.js
@@ -1,20 +1,21 @@
-var  mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
+document.addEventListener("click", function() {
+      // Add a click event listener to the document to create dots
+      document.addEventListener("click", function(event) {
+        if (event.target.className !== 'clear-button') {
+          var dot = document.createElement("div");
+          dot.className = "dot";
+          dot.style.left = (event.pageX - 4) + "px";
+          dot.style.top = (event.pageY - 4) + "px";
+          document.body.appendChild(dot);
+        }
+      });
 
-
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
-
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
+      // Add a click event listener to the clear button to remove all dots
+      var clearButton = document.querySelector(".clear-button");
+      clearButton.addEventListener("click", function() {
+        var dots = document.querySelectorAll(".dot");
+        dots.forEach(function(dot) {
+          dot.parentNode.removeChild(dot);
+        });
+      });
+    });
