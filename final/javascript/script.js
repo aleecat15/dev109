d9c678f57b47dd71a0a7365fbb3e76f8bbc69d66
aleecat15 <166102492+aleecat15@users.noreply.github.com>
2024-06-11 18:36:41 -0700

    Update script.js

--- final/javascript/script.js
+++ final/javascript/script.js
@@ -1,1 +1,27 @@
+  //Function
+  function validateForm() {
+    var fname = document.forms["myContact"]["fname"].value;
+    var email = document.forms["myContact"]["email"].value;
+    var comment = document.forms["myContact"]["comment"].value;
 
+    //First Name
+    if (fname == "" || !/^[a-zA-Z]+$/.test(fname) || fname.length >= 25) {
+      alert("Please enter a valid first name (letters only, less than 25 characters)");
+      return false;
+    }
+
+    //Email
+    var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email == "" || !emailPattern.test(email)) {
+      alert("Please enter a valid email address");
+      return false;
+    }
+
+    //Comment
+    if (comment == "" || comment.length < 1) {
+      alert("Please enter a comment");
+      return false;
+    }
+
+    return true; // Form validated
+  }
